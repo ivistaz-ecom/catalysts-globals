3b391a9b8655fd3a491aa8fe70e9708d6d5237a5
Kishorebarath Rajgopal <kishorebarathrajgopal@IVMBA-M2-Kishore-Laptop-21.local>
2024-12-11 09:55:21 +0530

    robots and sitemap updated now

--- app/organisations/page.jsx
+++ app/organisations/page.jsx
@@ -1,10 +1,24 @@
+"use client"
 import React from "react"
 import OrganisationsComponents from "@/components/OrganisationsComponents/"
-
+import Seo from "@/components/SeoComponents/Seo"
+import { usePathname } from "next/navigation"
 const Page = () => {
+  const pathname = usePathname()
+  const title = "Dedicated to enhancing the lives of the vulnerable"
+  const description =
+    "Learn more about the Catalyst Group of Organisations and how each brand is dedicated to the betterment of the society and in enhancing the lives of vulnerable communities."
+  const path = `${pathname}`
+  const metaImage = ""
   return (
     <div>
-      <OrganisationsComponents/>
+      <Seo
+        title={title}
+        description={description}
+        path={path}
+        metaImage={metaImage}
+      />
+      <OrganisationsComponents />
     </div>
   )
 }
--- app/partners/page.jsx
+++ app/partners/page.jsx
@@ -1,12 +1,28 @@
-import React from "react"
-import PartnersComponents from "@/components/PartnersComponents"
+"use client";
+import React from "react";
+import PartnersComponents from "@/components/PartnersComponents";
+import Seo from "@/components/SeoComponents/Seo";
+import { usePathname } from "next/navigation";
 
-const page = () => {
+const Page = () => {
+  const pathname = usePathname();
+  const title = "Partners that share our vision and passion";
+  const description =
+    "Our partners, who share our vision and passion, play a huge role in supporting us in every possible way to ensure we enhance the lives of vulnerable communities.";
+  const path = `${pathname}`;
+  const metaImage = "";
+
   return (
     <>
+      <Seo
+        title={title}
+        description={description}
+        path={path}
+        metaImage={metaImage}
+      />
       <PartnersComponents />
     </>
-  )
-}
+  );
+};
 
-export default page
+export default Page;
