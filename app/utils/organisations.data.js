b031cd477c6a99981fcad968f0b929e9fee3e270
Kishorebarath Rajgopal <kishorebarathrajgopal@IVMBA-M2-Kishore-Laptop-21.local>
2024-12-11 10:38:55 +0530

    organisation page updated

--- app/utils/organisations.data.js
+++ app/utils/organisations.data.js
@@ -1,86 +1,83 @@
 export const organisations = [
-    {
-      bgColor: "#622F88",
-      logoSrc: "/img/cms copy.png",
-      name: "Catalyst Management Services",
-      year: "(established in 1994)",
-      description: [
-        "A private limited organisation which specializes in providing consulting and professional services. CMS partners to create social impact, guaranteeing results through development solutions and investments.",
-        "CMS solutions range from Design (strategy and planning), Evidence (research, evaluation and insights), Monitoring (systems, accountability and performance), Learning systems (accretion and application).",
+  {
+    bgColor: "#622F88",
+    logoSrc: "/img/cms copy.png",
+    name: "Catalyst Management Services",
+    year: "(established in 1994)",
+    description: [
+      "A private limited organisation which specializes in providing consulting and professional services. CMS partners to create social impact, guaranteeing results through development solutions and investments. The organisation commits to sustainable development that leads to investments in ideas, individuals and institutions, creating shared value and maximizing social returns. CMS solutions range from Design (strategy and planning), Evidence (research, evaluation and insights), Monitoring (systems, accountability and performance), Learning systems (accretion and application).",
+      "CMS bridges worlds by creating partnerships across a spectrum of development actors – communities and their institutions, program implementers/ practitioners, financiers, policy influencers, researchers, networks, coalitions and platforms",
+    ],
+    link: "https://cms.org.in/",
+    rightImage: "/img/cms-image.jpg",
+  },
+  {
+    bgColor: "linear-gradient(to right, #ae2e6c, #2d356a, #8a7a2a)",
+    logoSrc: "/img/cat1.png",
+    name: "Impact Catalyst Foundation (ICF)",
+    year: "(established in 1994)",
+    description: [
+      "is a US public charity that is part of the Catalyst Group of organisations, headquartered in India. The Catalyst Group was founded in 1994 and is a social enterprise platform focused on the promotion of the health and wealth of vulnerable communities. Our work has spanned over 25 countries across Asia and Africa in multiple domains, including health, livelihood, and climate change.",
+    ],
+    link: "https://catalysts.global/impact-catalyst-foundation-icf",
+    rightImage: "/img/build_synergies.png",
+  },
+  {
+    bgColor: "#E2005A",
+    logoSrc: "/img/swasti-logo-1.png",
+    name: "Swasti",
+    year: "(established in 2004)",
+    description: [
+      "A registered Society (not-for-profit), Swasti – The Health Catalyst – is a health resource centre.Which achieves public health outcomes for the marginalised, through the delivery of end-to-end solutions as well as short and long term support and facilitation, combining research and practice.",
+      "Swasti – The Health Catalyst – was envisioned as an organisation to transform the lives of the marginalised communities by ensuring their access to quality healthcare. Swasti's work has fuelled impact by promoting healthcare access thereby contributing to poverty alleviation. Swasti have supported governments, donors and development partners to design solutions in public health. Swasti has directly reached over 400,000 people through our various initiatives.",
+    ],
+    link: "https://swasti.org/",
+    rightImage: "/img/swasti-img.jpg",
+  },
+  {
+    bgColor: "#004534",
+    logoSrc: "/img/vrutti-logo-1-1.png",
+    name: "Vrutti",
+    year: "(established in 2002)",
+    description: [
+      "A registered Society (not-for-profit), Vrutti is a Livelihood Impact Partner which enhances people's well-being through knowledge, innovation and transformative actions and brings about solutions at a scale that eliminate inequities and create wealth for the marginalised. Vrutti is committed to finding solutions to end poverty, marginalisation and creation of wealth and build resilience for small producers. As social organisation, Vrutti pursues problem solving with entrepreneurial zeal, business acumen, courage to innovate, and consistently challenge traditional practices. Vrutti is passionate about unbiased measurement, self-critique and learning. Vrutti works through result-based planning tools, performance measurement metrics, impact and value for money assessments and use technology as augmenter.",
+
+    ],
+    link: "https://vruttiimpactcatalysts.org/",
+    rightImage: "/img/vrutti-img.jpg",
+  },
+  {
+    bgColor: "#FAFAFA",
+    logoSrc: "/img/Fuzhioi-logo-1.png",
+    name: "Fuzhio",
+    year: "(established in 2005)",
+    description: [
+      "Facilitating Market Access to Women and Smallholder Farmers and Marginalised Communities to Maximise Returns for their Products and Services. Fuzhio promotes Impact Products directly to customers on behalf of small farmers and vulnerable groups and allows them to maximize yields and provide customers with safe, responsible products.",
+    ],
+    link: "https://fuzhio.org/",
+    rightImage: "/img/fuzhio-img.jpg",
+  },
+  {
+    bgColor: "#FFFFFF",
+    logoSrc: "/img/GREEN-FOUNDATION-LOGO.png",
+    name: "Green Foundation",
+    year: "(established in 1994)",
+    description: [
+      "GREEN Foundation works toward a well-preserved, diverse ecosystem that will sustain the rural livelihoods of the present generation without eroding the resource base of the future. Our work to empower the small-scale and marginal farmers of India first began in 1994 with a simple effort to distribute indigenous seed varieties in and around the village of Thalli, Tamil Nadu. This soon turned into a growing biodiversity conservation program that has been central to our work ever since.",
       ],
-      link: "https://cms.org.in/",
-      rightImage: "/img/cms-image.jpg",
-    },
-    {
-      bgColor: "linear-gradient(to right, #ae2e6c, #2d356a, #8a7a2a)",
-      logoSrc: "/img/cat1.png",
-      name: "Impact Catalyst Foundation (ICF)",
-      year: "(established in 1994)",
-      description: [
-        "A US public charity that is part of the Catalyst Group of organisations. Focused on the promotion of the health and wealth of vulnerable communities.",
-        "Our work has spanned over 25 countries across Asia and Africa in multiple domains, including health, livelihood, and climate change.",
-      ],
-      link: "https://catalysts.global/impact-catalyst-foundation-icf",
-      rightImage: "/img/build_synergies.png",
-    },
-    {
-      bgColor: "#E2005A",
-      logoSrc: "/img/swasti-logo-1.png",
-      name: "Swasti",
-      year: "(established in 2004)",
-      description: [
-        "Swasti is a health resource centre achieving public health outcomes for the marginalised through the delivery of end-to-end solutions and facilitation.",
-        "Swasti’s work fuels impact by promoting healthcare access, contributing to poverty alleviation, and supporting governments, donors, and development partners.",
-      ],
-      link: "https://swasti.org/",
-      rightImage: "/img/swasti-img.jpg",
-    },
-    {
-      bgColor: "#004534",
-      logoSrc: "/img/vrutti-logo-1-1.png",
-      name: "Vrutti",
-      year: "(established in 2002)",
-      description: [
-        "Vrutti is a Livelihood Impact Partner enhancing well-being through knowledge, innovation, and transformative actions.",
-        "Vrutti pursues problem-solving with entrepreneurial zeal to eliminate inequities and create wealth for the marginalised.",
-      ],
-      link: "https://vruttiimpactcatalysts.org/",
-      rightImage: "/img/vrutti-img.jpg",
-    },
-    {
-      bgColor: "#FAFAFA",
-      logoSrc: "/img/Fuzhioi-logo-1.png",
-      name: "Fuzhio",
-      year: "(established in 2005)",
-      description: [
-        "Facilitating market access for women, smallholder farmers, and marginalised communities to maximise returns for their products and services.",
-        "Fuzhio promotes impact products directly to customers on behalf of small farmers and vulnerable groups.",
-      ],
-      link: "https://fuzhio.org/",
-      rightImage: "/img/fuzhio-img.jpg",
-    },
-    {
-      bgColor: "#FFFFFF",
-      logoSrc: "/img/GREEN-FOUNDATION-LOGO.png",
-      name: "Green Foundation",
-      year: "(established in 1994)",
-      description: [
-        "GREEN Foundation works toward a well-preserved, diverse ecosystem that will sustain the rural livelihoods of the present generation without eroding the resource base of the future.",
-        "Our work to empower the small-scale and marginal farmers of India first began in 1994 with a simple effort to distribute indigenous seed varieties in and around the village of Thalli, Tamil Nadu. This soon turned into a growing biodiversity conservation program that has been central to our work ever since.",
-      ],
-      link: "https://greenfoundation.in/",
-      rightImage: "/img/Feature-Image-1.png",
-    },
-    {
-      bgColor: "#000000",
-      logoSrc: "/img/logo-1.png",
-      name: "Upfront",
-      year: "(established in 2022)",
-      description: [
-        "Upfront works with front line workers and the 'systems' by changing mindsets, behaviours and practices; improving skills, financial security, processes and services; building agency, ensuring voices and choices; and establishing partnerships and collaborations.",
-        "Creating a positive impact on the workers' ecosystem by leveraging a systems change approach while ensuring knowledge, skills (personal and professional), and services (health and wealth) through a platform approach that aggregates solutions and providers.",
-      ],
-      link: "https://upfront.global/",
-      rightImage: "/img/Feature-Image.png",
-    },
-  ]
+    link: "https://greenfoundation.in/",
+    rightImage: "/img/Feature-Image-1.png",
+  },
+  {
+    bgColor: "#000000",
+    logoSrc: "/img/logo-1.png",
+    name: "Upfront",
+    year: "(established in 2022)",
+    description: [
+      "Upfront works with front line workers and the 'systems' by changing mindsets, behaviours and practices; improving skills, financial security, processes and services; building agency, ensuring voices and choices; and establishing partnerships and collaborations.",
+      "Creating a positive impact on the workers' ecosystem by leveraging a systems change approach while ensuring knowledge, skills (personal and professional), and services (health and wealth) through a platform approach that aggregates solutions and providers.",
+    ],
+    link: "https://upfront.global/",
+    rightImage: "/img/Feature-Image.png",
+  },
+]
