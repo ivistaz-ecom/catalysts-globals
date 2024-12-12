7b38b6ab74fb474c98aa58d73a61244ccb66c613
Kishorebarath Rajgopal <kishorebarathrajgopal@IVMBA-M2-Kishore-Laptop-21.local>
2024-12-12 14:19:02 +0530

    header page navitem and mobile responsive updated

--- components/SharedComponents/Header/Header.js
+++ components/SharedComponents/Header/Header.js
@@ -125,7 +125,7 @@ const Header = () => {
           aria-label="Toggle Menu"
         >
           <i
-            className={`bi ${isMenuOpen ? "bi-x-lg" : "bi-list"} text-2xl`}
+            className={`bi ${isMenuOpen ? "bi-x-lg" : "bi-list"} text-4xl`}
           ></i>
         </button>
 
