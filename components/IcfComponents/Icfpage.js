af51ac365a7a1ad3a9f4d90c5137b52c6aba14a2
Kishorebarath Rajgopal <kishorebarathrajgopal@IVMBA-M2-Kishore-Laptop-21.local>
2025-01-02 21:27:51 +0530

    icfpage updated done

--- components/IcfComponents/Icfpage.js
+++ components/IcfComponents/Icfpage.js
@@ -133,7 +133,7 @@ const Icfpage = () => {
       {/* Content Section */}
       <div
 
-        className="relative z-10 flex flex-col items-center justify-center flex-1 px-4 py-7"
+        className="relative z-10 flex flex-col items-center justify-center flex-1 px-3 py-7"
         id="Icfpage"
       >
         <Image
