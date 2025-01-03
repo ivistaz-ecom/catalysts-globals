8a8afdf2839285bf8836711e451a3db0fcc298eb
Kishorebarath Rajgopal <kishorebarathrajgopal@IVMBA-M2-Kishore-Laptop-21.local>
2025-01-03 14:16:02 +0530

    mobile responsive updated in ifcpage

--- components/IcfComponents/Icfpage.js
+++ components/IcfComponents/Icfpage.js
@@ -40,7 +40,7 @@ const Icfpage = () => {
       viewBox="0 0 24 24"
       strokeWidth="2"
       stroke="currentColor"
-      className="w-6 h-6"
+      className="w-8 h-8"
     >
       <path
         strokeLinecap="round"
@@ -55,16 +55,17 @@ const Icfpage = () => {
       viewBox="0 0 24 24"
       strokeWidth="2"
       stroke="currentColor"
-      className="w-6 h-6"
+      className="w-8 h-8"
     >
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
-        d="M4 6h16M4 12h16m-7 6h7"
+        d="M4 6h16M4 12h16M4 18h16"  // Ensures three lines are visible
       />
     </svg>
   )}
 </button>
+
 
 
           {/* Desktop Menu */}
